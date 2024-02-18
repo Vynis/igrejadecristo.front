d3c374bec3e6beea501ff3c923b906444dff49a7
Vinícius de Oliveira Sousa Castro <vynis2005@gmail.com>
2024-02-18 11:41:04 -0300

    culo ini

---
+++ culto/src/app/model/cronograma.model.ts
@@ -0,0 +1,13 @@
+export class CronogramaModel{
+    qtdMinutos: number;
+    secao: string;
+
+
+    /**
+     *
+     */
+    constructor() {
+       this.qtdMinutos = 0;
+       this.secao = '';
+    }
+}
